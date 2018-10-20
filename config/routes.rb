4989804809e5e313c72132552534364aef8f0c61
Joemutt <joemutt10@gmail.com>
2018-10-20 19:12:28 +0300

    Fix bugs,Search ation and errors, Add and Remove friends

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'my_friends', to: 'users#my_friends'
   resources :users, only: [:show]
   resources :friendships
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
